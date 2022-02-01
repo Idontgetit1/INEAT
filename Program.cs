ca1d7ff7b2c36fd535a0401e45ba1f9e20b891fa
Idontgetit1 <thumbenjamin@gmail.com>
2022-02-01 16:27:23 +0100

    mutation Test

--- Program.cs
+++ Program.cs
@@ -16,7 +16,9 @@ namespace INEAT
             genome.addNodeGene(new NodeGene(1));
             genome.addNodeGene(new NodeGene(2));
 
+            genome.addConnectionGene(new ConnectionGene(0, 1, 1, true, genome.innovation.getInnovation()));
             genome.addConnectionGene(new ConnectionGene(1, 2, 1, true, genome.innovation.getInnovation()));
+
 
             // print genome connections
             Console.WriteLine("Genome connections:");
@@ -25,8 +27,7 @@ namespace INEAT
                 Console.WriteLine("inNode: " + connection.inNode + " outNode: " + connection.outNode + " weight: " + connection.weight + " expressed: " + connection.expressed + " innovation: " + connection.innovation);
             }
 
-            genome.addConnectionMutation(0, 2);
-            genome.addNodeMutation(1);
+            genome.mutation();
 
             // print genome connections
             Console.WriteLine("Genome connections:");
